11b42f37b0e452b56741f34e54cc8381f58504a0
Francisco Gimenez <88054487+FranGimenez98@users.noreply.github.com>
2022-11-14 20:41:28 -0300

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,5 +3,6 @@ const router = Router();
 
 router.use('/example', require('./example.route'))
 router.use("/user", require("./user.route"))
+router.use("/post", require("./post.route"));
 
 module.exports = router;

1094ff971643a7a707220a3cedc887b14a87a786
Mateo del Campillo <mateodelcampillo01@gmail.com>
2022-11-14 20:15:52 -0300

    authUserRoutes and registration done

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,5 +2,6 @@ const Router = require("express");
 const router = Router();
 
 router.use('/example', require('./example.route'))
+router.use("/user", require("./user.route"))
 
 module.exports = router;

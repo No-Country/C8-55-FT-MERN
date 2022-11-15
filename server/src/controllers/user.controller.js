ea1b84dc446e80ad5b52f8c9e78e346629775a1c
Mateo del Campillo <mateodelcampillo01@gmail.com>
2022-11-15 15:33:22 -0300

    Typing error fix

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const User = require("../Models/User");
+const User = require("../models/User");
 const _ = require("lodash");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,4 +1,4 @@
-const User = require("../Models/User");
+const User = require("../models/User");
 const Router = require("express");
 const { signUp, signIn } = require("../controllers/user.controller");
 const router = Router();

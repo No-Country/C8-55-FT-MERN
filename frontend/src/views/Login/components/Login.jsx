c24d852bb27d9f93b1915d01867bc4ea0135ec58
Deriamsuarez <45581669+Deriamsuarez@users.noreply.github.com>
2022-11-16 10:18:30 -0400

    all

--- frontend/src/views/Login/components/Login.jsx
+++ frontend/src/views/Login/components/Login.jsx
@@ -1,58 +1,83 @@
 import { Box, Divider, Stack, TextField, Typography, ButtonGroup, Button } from '@mui/material'
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 
-const Login = ({setLogStatus}) => {
+import axios from 'axios'
+import { setUser } from '../../../store/slices/user.slice'
+
+const Login = ({ setLogStatus }) => {
 
   const [log, setLog] = useState()
+  console.log(log)
+
+  const user = useSelector(state => state.user)
+
+  console.log(user)
+  const dispatch = useDispatch()
+
+  const getUser = user => {
+    axios.post('http://localhost:3000/user/signin', user)
+      .then(res => {
+        console.log(res.data)
+        localStorage.setItem('token', res.data.token)
+        dispatch(setUser(res.data))  
+      })
+      .catch(err => console.log(err.response))
+  }
+
+  // useEffect(() => {
+  //   getUser();
+  // }, [log])
 
 
   const loginUser = e => {
     e.preventDefault();
 
     const userData = {
-      name: e.target.mailLogin.value.trim(),
-      lastname: e.target.passwordLogin.value.trim(),
+      mail: e.target.mailLogin.value.trim(),
+      password: e.target.passwordLogin.value.trim(),
     }
     setLog(userData)
+    getUser(userData)
 
   }
 
 
   return (
-    <Stack component='form' onSubmit={loginUser} p='2em' sx={{ width: '350px', borderRadius: '0.8em', backgroundColor: 'white', gap: '1em'}} >
-      <Box sx={{ textAlign: 'center'}}>
+    <Stack component='form' onSubmit={loginUser} p='2em' sx={{ width: '350px', borderRadius: '0.8em', backgroundColor: 'white', gap: '1em' }} >
+      <Box sx={{ textAlign: 'center' }}>
         <Typography variant="h5" color="#FF9F1C"><strong>Hello again!</strong></Typography>
         <Typography variant="subtitle" color="initial">Wellcome back you ha've been missed</Typography>
       </Box>
       <Box sx={{ widht: '100%', display: 'flex', flexDirection: 'column', gap: '1em' }}>
         <Divider />
         <TextField
-        required
+          required
           label="email"
           id="mailLogin"
           size="small"
+          type='mail'
           fullWidth
         />
         <TextField
-        required
+          required
           label="password"
           id="passwordLogin"
           size="small"
           fullWidth
           type='password'
         />
-  
+
       </Box>
-      <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
-      <Button variant='text' color='warning' onClick={()=> setLogStatus()}>Sign up</Button>
-      <Button variant='contained' color='warning' type='submit'>Log In</Button>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Button variant='text' color='warning' onClick={() => setLogStatus()}>Sign up</Button>
+        <Button variant='contained' color='warning' type='submit'>Log In</Button>
 
       </Box>
       <Divider />
       <Button variant="text" color="primary">
-          Forgot password?
-        </Button>
+        Forgot password?
+      </Button>
     </Stack>
   )
 }

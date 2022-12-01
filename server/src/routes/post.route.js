ca36677373650cceb73509038066079e2417f183
franprogweb98 <franprogweb98@gmail.com>
2022-12-01 16:04:51 -0300

    fixed posts routes

--- server/src/routes/post.route.js
+++ server/src/routes/post.route.js
@@ -4,10 +4,8 @@ const Router = require("express");
 const router = Router();
 
 router.post("/create", verifyToken, postController.createPost);
-router.get("/all_posts", postController.getPosts);
-// router.get("/all_posts", verifyToken, postController.getPosts);
-router.get("/user_posts/:id", postController.getUserPosts);
-// router.get("/user_posts/:id", verifyToken, postController.getUserPosts);
+router.get("/all_posts", verifyToken, postController.getPosts);;
+router.get("/user_posts/:id", verifyToken, postController.getUserPosts);
 router.get("/get_post/:id", verifyToken, postController.getPostById);
 router.delete("/delete/:id", verifyToken, postController.deletePost);
 router.put("/update/:id", verifyToken, postController.updatePost);

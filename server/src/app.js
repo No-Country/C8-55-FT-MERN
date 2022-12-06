93885bf1208fb3f28e0e2deaff9e9a4a9c6306c1
franprogweb98 <franprogweb98@gmail.com>
2022-12-06 16:10:29 -0300

    Feature: new testing routes

--- server/src/app.js
+++ server/src/app.js
@@ -4,33 +4,14 @@ const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const { urlencoded } = require("body-parser");
-const db = require("./database/db.config");
 const routes = require("./routes/index");
 const { PORT } = require("./config");
-const path = require("path");
 
 //swagger
 const swaggerUI = require("swagger-ui-express");
 const documentation = require("./helpers/documentation");
-const { socketOn } = require("./socketIo");
 
 const app = express();
-const http = require("http")
-  .createServer(app)
-  .listen(PORT, () => {
-    console.log(`listening on port http://localhost:${PORT}/ ​​​🤟​😎​`);
-  });
-const io = require("socket.io")(http, {
-  cors: {
-    origin: "http://localhost:5173",
-  },
-});
-
-socketOn(io);
-
-db().then(() => {
-  console.log("connected to database");
-});
 
 //middlewares
 app.use(cors());
@@ -51,4 +32,4 @@ app.use((err, req, res, next) => {
   return;
 });
 
-module.exports = { io };
+module.exports = { app };
---
+++ server/src/server.js
@@ -0,0 +1,23 @@
+const { socketOn } = require("./socketIo");
+const db = require("./database/db.config");
+const { app } = require("./app");
+const { PORT, NODE_ENV } = require("./config");
+
+const http = require("http")
+  .createServer(app)
+  .listen(PORT, () => {
+    console.log(`listening on port http://localhost:${PORT}/ ​​​🤟​😎​`);
+  });
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+socketOn(io);
+
+db().then(() => {
+  console.log("connected to database");
+});
+
+module.exports = { io };
